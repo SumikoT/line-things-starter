8f1e63f4a0adc33ad1926e90167c31ab9bcb0f42
TAODRIVE\sumiko-tohyama <sumiko-tohyama@taodrive.com>
2020-03-26 19:45:48 +0900

    try_things2 regist

--- esp32/arduino/sample/sample.ino
+++ esp32/arduino/sample/sample.ino
@@ -7,7 +7,7 @@
 #define DEVICE_NAME "LINE Things Trial ESP32"
 
 // User service UUID: Change this to your generated service UUID
-#define USER_SERVICE_UUID "d298cde0-da73-49b2-ace2-2f93c4df8893"
+#define USER_SERVICE_UUID "f7203392-4c58-4f9f-8707-30b34e0f8eb1"
 // User service characteristics
 #define WRITE_CHARACTERISTIC_UUID "E9062E71-9E62-4BC6-B0D3-35CDCD9B027B"
 #define NOTIFY_CHARACTERISTIC_UUID "62FBD229-6EDD-4D1A-B554-5C4E1BB29169"
--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID = 'd298cde0-da73-49b2-ace2-2f93c4df8893'; // LED, Button
+const USER_SERVICE_UUID = 'f7203392-4c58-4f9f-8707-30b34e0f8eb1'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
